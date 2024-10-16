1bc4acc87f3543051814e9ccabbcd9c199d3e8d9
writemorecode <gustav.ae.karlsson@gmail.com>
2024-10-16 17:27:13 +0200

    Moved code back into macro form

--- src/auth/oauth/local.rs
+++ src/auth/oauth/local.rs
@@ -3,6 +3,7 @@ use crate::error::ServerResponseError;
 use crate::state::AppState;
 use actix_web::http::header::CacheDirective;
 use actix_web::{http::header, web, HttpResponse, Scope};
+use helper_macros::generate_endpoint;
 use oauth2::{AccessToken, RefreshToken};
 use rand::{
     distributions::{Alphanumeric, DistString},
@@ -53,39 +54,50 @@ pub struct Foo {
     password: Option<String>,
 }
 
-#[utoipa::path(post,path = "/token",context_path = "/local",tag = "token",responses((status = 200u16,description = "Local OAuth provider token endpoint")))]
-#[actix_web::post("/token")]
-pub async fn local_token(
-    state: web::Data<AppState>,
-    data: web::Form<TokenRequest>,
-) -> Result<impl ::actix_web::Responder, crate::error::ServerResponseError> {
-    info!("Recieved token request: {:?}", &data);
+generate_endpoint! {
+    fn local_token;
+    method: post;
+    path: "/token";
+    docs: {
+        tag: "token",
+        context_path: "/local",
+        responses: {
+            (status = 200, description = "Local OAuth provider token endpoint")
+        }
+    }
+    params: {
+        state: web::Data<AppState>,
+        data: web::Form<TokenRequest>,
+    };
+    {
+        info!("Recieved token request: {:?}", &data);
 
-    match data.into_inner() {
-        TokenRequest::RefreshToken { refresh_token } => Err(ServerResponseError::BadRequest(
-            "Refreshing tokens not yet supported".to_string(),
-        )),
-        TokenRequest::Password { username, password } => {
-            let query =
-                r#"SELECT COUNT() FROM user WHERE username = $username AND password = $password;"#;
-            let query_result: Option<i32> = state
-                .db
-                .query(query)
-                .bind(("username", username))
-                .bind(("password", password))
-                .await?
-                .take("count")?;
+        match data.into_inner() {
+            TokenRequest::RefreshToken { refresh_token } => Err(ServerResponseError::BadRequest(
+                "Refreshing tokens not yet supported".to_string(),
+            )),
+            TokenRequest::Password { username, password } => {
+                let query =
+                    r#"SELECT COUNT() FROM user WHERE username = $username AND password = $password;"#;
+                let query_result: Option<i32> = state
+                    .db
+                    .query(query)
+                    .bind(("username", username))
+                    .bind(("password", password))
+                    .await?
+                    .take("count")?;
 
-            let valid_user = query_result.is_some_and(|count| count > 0);
-            if valid_user {
-                Ok(HttpResponse::Ok()
-                    .insert_header(header::CacheControl(vec![
-                        CacheDirective::NoCache,
-                        CacheDirective::NoStore,
-                    ]))
-                    .json(TokenResponse::new()))
-            } else {
-                Err(ServerResponseError::NotFound)
+                let valid_user = query_result.is_some_and(|count| count > 0);
+                if valid_user {
+                    Ok(HttpResponse::Ok()
+                        .insert_header(header::CacheControl(vec![
+                            CacheDirective::NoCache,
+                            CacheDirective::NoStore,
+                        ]))
+                        .json(TokenResponse::new()))
+                } else {
+                    Err(ServerResponseError::NotFound)
+                }
             }
         }
     }
