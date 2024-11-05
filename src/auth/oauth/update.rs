2c6eea5f780299cbbb8e784b34b5154f0070fbd6
writemorecode <gustav.ae.karlsson@gmail.com>
2024-11-05 16:40:10 +0100

    Finished endpoint for updating user info and/or password

--- src/auth/oauth/update.rs
+++ src/auth/oauth/update.rs
@@ -1,26 +1,25 @@
-use crate::auth::users::auth::UserAuth;
 use crate::auth::users::get::utils::get_user_by_token;
-use crate::auth::UserInfo;
 use crate::error::ServerResponseError;
-use crate::extractors::{Auth, IntoSession};
+use crate::extractors::AuthenticatedToken;
+use crate::extractors::IntoSession;
 use crate::generate_endpoint;
 use crate::state::AppState;
-use actix_web::{web, Either, HttpResponse};
+use actix_web::{web, HttpResponse};
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use serde::{Deserialize, Serialize};
-
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
-pub struct UserUpdateRequest {
+pub struct UserInfoUpdateRequest {
     pub username: String,
     pub first_name: String,
     pub last_name: String,
-    pub password: Option<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Password {
-    data: String,
+#[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
+pub struct UserUpdateRequest {
+    #[serde(flatten)]
+    pub user_info_update: Option<UserInfoUpdateRequest>,
+    pub password: Option<String>,
 }
 
 generate_endpoint! {
@@ -42,46 +41,28 @@ generate_endpoint! {
     }
     params: {
         data: web::Json<UserUpdateRequest>,
-        token: Auth,
+        token: AuthenticatedToken,
         state: web::Data<AppState>,
     };
     {
-        let session = match token {
-            Either::Left(identity) => {
-                let session = identity.get_session().await;
+        let access_token = token.get_token();
+        let user = get_user_by_token(&state.db, &access_token).await?;
 
-                if session.is_none() {
-                    return Err(ServerResponseError::Unauthorized);
-                }
+        let Some(user_id) = user.id else {return Err(ServerResponseError::NotFound); };
 
-                session.unwrap()
-            },
-            Either::Right(session) => {
-                session
-            },
-        };
         let update_data = data.into_inner();
-        let user = get_user_by_token(&state.db, &session.access_token).await?;
-        dbg!(&user);
 
-        // This unwrap is ugly!
-        // A 'UserInfo' returned from the database should have a user ID field with type Thing
-        // and not type Option<Thing>
-        let user_id = user.id.unwrap();
-        dbg!(&user_id);
-
-        let updated_user: Option<UserInfo> = state.db.query("UPDATE $user_id MERGE $user_data")
-            .bind(("user_id", user_id.clone()))
-            .bind(("user_data", update_data.clone())).await?.take(0)?;
-        dbg!(updated_user);
+        if let Some(user_info_update) = update_data.user_info_update {
+            state.db.query("UPDATE $user_id MERGE $user_data")
+                .bind(("user_id", user_id.clone()))
+                .bind(("user_data", user_info_update)).await?;
+        }
 
         if let Some(password) = update_data.password {
-            // I don't like that I have to use "CONTAINS" here, can probably be improved.
-            let updated_user_auth: Option<UserAuth> = state.db
+            state.db
                 .query("UPDATE user_auth SET password = $new_password WHERE ->auth_for->user.id CONTAINS $user_id;")
                 .bind(("new_password", password))
-                .bind(("user_id", user_id)).await?.take(0)?;
-            dbg!(updated_user_auth);
+                .bind(("user_id", user_id)).await?;
         }
 
         Ok(HttpResponse::Ok().finish())
