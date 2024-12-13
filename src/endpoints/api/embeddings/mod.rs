67ec8ff7e0c37040405b0ff93b8560fa8c496060
writemorecode <gustav.ae.karlsson@gmail.com>
2024-12-13 13:15:47 +0100

    Removed limit on size of JSON payloads to embedding service

--- src/endpoints/api/embeddings/mod.rs
+++ src/endpoints/api/embeddings/mod.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{
+    get, post,
+    web::{self, JsonConfig},
+    HttpResponse, Responder,
+};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -50,7 +54,10 @@ async fn search_embeddings(
 }
 
 pub fn embeddings_service() -> impl actix_web::dev::HttpServiceFactory {
+    let json_config = JsonConfig::default().limit(usize::MAX);
+
     web::scope("/embeddings")
+        .app_data(json_config)
         .service(add_embeddings)
         .service(search_embeddings)
 }
