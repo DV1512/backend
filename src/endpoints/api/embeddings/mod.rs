f05666d48c995214481c4b1c7d98ecd9c0fc4a27
writemorecode <gustav.ae.karlsson@gmail.com>
2024-12-14 12:55:19 +0100

    Set max JSON payload size to unlimited globally

--- src/endpoints/api/embeddings/mod.rs
+++ src/endpoints/api/embeddings/mod.rs
@@ -1,14 +1,11 @@
-use actix_web::web::{self, JsonConfig};
+use actix_web::web;
 use serde::{Deserialize, Serialize};
 
 mod post;
 use post::*;
 
 pub fn embeddings_service() -> impl actix_web::dev::HttpServiceFactory {
-    let json_config = JsonConfig::default().limit(usize::MAX);
-
     web::scope("/embeddings")
-        .app_data(json_config)
         .service(add_embeddings)
         .service(search_embeddings)
 }
